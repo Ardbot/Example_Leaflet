ef08409f6c56d2d9263903f4ee925c890368677b
Ardbot <45563576+Ardbot@users.noreply.github.com>
2022-09-27 21:10:54 +0900

    Коммиты

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var start_point = [50.253313, 127.8073497]
 
 // Создаем пустую карту, . 
 var map = L.map('map', { // Используется библиотека Leaflet
-  minZoom: 10, // Минимальный масштаб карты (min 0) (действует неа всю карту)
-  // maxZoom: 20 // Максимальный мастштаб (max >=21) (действует неа всю карту)
+  minZoom: 10, // Минимальный масштаб карты (min 0) (действует на всю карту) https://wiki.openstreetmap.org/wiki/RU:Zoom_levels
+  maxZoom: 20// Максимальный мастштаб (max >=21) (действует на всю карту)
 }).setView(start_point, 13); // Устанавливаем на старте центральную точку и масштаб (13)
 
 // Подключение базовых слоев (подложек)
@@ -13,5 +13,10 @@ var tiles_OSM = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
   opacity: 1.0, // Прозрачность базового слоя
   attribution: '<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   minZoom: 0, // Минимальный масштаб карты (min 0)
-  // maxZoom: 20 // Максимальный масштаб карты (max >=21)
+  maxZoom: 19 // Максимальный масштаб карты (max =19)
 }).addTo(map) // Добавляем на карту.
+
+// Выводит текущий зум
+map.on('zoomend', function () {
+  $("#zoom").text(map.getZoom())
+});
